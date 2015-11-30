88536cae45e4fa36b3a1c2bf163c1ea9bd320642
Rexon A. De los Reyes <rexondelosreyes88@gmail.com>
2015-11-30 13:44:46 -0800

    Customs about. yeah

---
+++ www/js/controllers/AboutController.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('AppControllers')
+        .controller('aboutCtrl', function ($scope, $timeout, ionicMaterialMotion, oResume) {
+
+
+            var vm = $scope;
+            vm.about = oResume.about;
+
+            $timeout(function() {
+                ionicMaterialMotion.ripple({
+                    startVelocity: 3000
+                });
+            }, 700);
+        });
+
+})();
---
+++ www/js/controllers/ContactsController.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('AppControllers')
+        .controller('contactsCtrl', function ($scope, oResume) {
+
+            var vm = $scope;
+            vm.contacts = oResume.basics;
+        });
+
+})();
