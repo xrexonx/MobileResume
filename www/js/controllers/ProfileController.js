8f23278601062fd81f825f5099ec859c2e686416
Rexon A. De los Reyes <rexondelosreyes88@gmail.com>
2015-11-30 09:21:05 -0800

    Awesome profile

--- www/js/controllers/ProfileController.js
+++ www/js/controllers/ProfileController.js
@@ -3,14 +3,27 @@
 
     angular
         .module('AppControllers')
-        .controller('profileCtrl', function ($scope, oResume, ionicMaterialInk, ionicMaterialMotion) {
+        .controller('profileCtrl', function ($scope, $timeout, oResume, ionicMaterialInk, ionicMaterialMotion) {
 
-            //console.log(ionicMaterialInk);
-            //console.log(ionicMaterialMotion);
-
-            ionicMaterialMotion.slideUp('xx');
-            var vm = $scope
+            var vm = $scope;
             vm.resume = oResume.basics;
+
+            // Set Motion
+            $timeout(function() {
+                ionicMaterialMotion.slideUp({
+                    selector: '.slide-up'
+                });
+            }, 300);
+
+
+            $timeout(function() {
+                ionicMaterialMotion.fadeSlideInRight({
+                    startVelocity: 3000
+                });
+            }, 700);
+
+            // Set Ink
+            ionicMaterialInk.displayEffect();
 
         });
 
