c4c9adc810299fb537fb9df86558414296ea9e9b
rexon <rexondelosreyes88@gmail.com>
2015-12-01 09:18:56 +0800

    Education and Experience template refactor

--- www/js/controllers/EducationController.js
+++ www/js/controllers/EducationController.js
@@ -9,7 +9,7 @@
             vm.education = oResume.education;
 
             $timeout(function () {
-                ionicMaterialMotion.fadeSlideInRight();
+                ionicMaterialMotion.fadeSlideIn();
             }, 300);
 
         });
