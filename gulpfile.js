8a945e29d465a274e52d0994e1aa9fa49d67624f
rexon <rexondelosreyes88@gmail.com>
2015-12-03 12:06:45 +0800

    Wew, added gulp tasks runner for js files

--- gulpfile.js
+++ gulpfile.js
@@ -6,39 +6,69 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var uglify = require('gulp-uglify');
+var ngmin = require('gulp-ngmin');
+
+var dest = {
+  css: './www/css/',
+  js: './www/build/js/'
+};
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  js: {
+    controllers: ['./www/js/controllers/*.js'],
+    services: ['./www/js/services/*.js']
+  }
 };
 
 gulp.task('default', ['sass']);
 
-gulp.task('sass', function(done) {
+gulp.task('sass', function (done) {
   gulp.src('./scss/ionic.app.scss')
     .pipe(sass({
       errLogToConsole: true
     }))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest(dest.css))
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(rename({extname: '.min.css'}))
+    .pipe(gulp.dest(dest.css))
     .on('end', done);
 });
 
-gulp.task('watch', function() {
+gulp.task('scripts', function () {
+
+  var oPath = paths.js;
+
+  for (var key in oPath) {
+    if (oPath.hasOwnProperty(key)) {
+      var path = oPath[key];
+      var files = 'App' + key + '.min.js';
+       gulp
+        .src(path)
+        .pipe(concat(files))
+        .pipe(ngmin({dynamic: true}))
+        .pipe(uglify())
+        .pipe(gulp.dest(dest.js));
+    }
+  }
+
+});
+
+gulp.task('watch', function () {
   gulp.watch(paths.sass, ['sass']);
 });
 
-gulp.task('install', ['git-check'], function() {
+gulp.task('install', ['git-check'], function () {
   return bower.commands.install()
-    .on('log', function(data) {
+    .on('log', function (data) {
       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
     });
 });
 
-gulp.task('git-check', function(done) {
+gulp.task('git-check', function (done) {
   if (!sh.which('git')) {
     console.log(
       '  ' + gutil.colors.red('Git is not installed.'),
--- www/js/app.js
+++ www/js/app.js
@@ -12,7 +12,6 @@
                 if (window.cordova && window.cordova.plugins.Keyboard) {
                     cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
                     cordova.plugins.Keyboard.disableScroll(true);
-
                 }
                 if (window.StatusBar) {
                     // org.apache.cordova.statusbar required
